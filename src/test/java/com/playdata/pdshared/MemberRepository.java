665ef68d784a79deef6bc779860aa34eb97cbe80
dokyeong99 <dlehrud451@gmail.com>
2023-08-02 15:57:06 +0900

    restart

--- src/test/java/com/playdata/pdshared/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.playdata.pdshared;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-    Optional<Member> findMemberByProviderId(String providerId);
-}
--- src/test/java/com/playdata/pdshared/config/token/TokenRequired.java
+++
@@ -1,7 +0,0 @@
-package com.playdata.pdshared.config.token;
-
-public @interface TokenRequired {
-
-
-
-}
