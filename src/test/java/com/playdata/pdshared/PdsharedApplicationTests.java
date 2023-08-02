e770d8277b0f00f2b23a25ea54698343fa74a414
dokyeong99 <dlehrud451@gmail.com>
2023-08-02 15:59:11 +0900

    rerestart

---
+++ src/test/java/com/playdata/pdshared/PdsharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.pdshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
