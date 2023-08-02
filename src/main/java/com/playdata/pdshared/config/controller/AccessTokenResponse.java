1fe34aa05b3e5d68009ffc51adc6f69d593bf3ef
dokyeong99 <dlehrud451@gmail.com>
2023-08-02 14:31:06 +0900

    a

---
+++ src/main/java/com/playdata/pdshared/config/controller/AccessTokenResponse.java
@@ -0,0 +1,14 @@
+package com.playdata.pdshared.config.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccessTokenResponse {
+    private String access_token;
+
+
+}
--- src/main/java/com/playdata/pdshared/config/token/TokenAspect.java
+++ src/main/java/com/playdata/pdshared/config/token/TokenAspect.java
@@ -1,33 +1,22 @@
 package com.playdata.pdshared.config.token;
-//
-//
-//import lombok.RequiredArgsConstructor;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
-//
-//@Aspect
-//@RequiredArgsConstructor
-public class TokenAspect {
-}
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-//    private final TokenService tokenService;
-//    @Before("@annotation(TokenRequired)")
-//    public void TokenCheck(){
-//        //Todo 토큰 없으면 로그인 페이지로 가게함
-//        ServletRequestAttributes request =
-//                (ServletRequestAttributes) RequestContextHolder
-//                        .currentRequestAttributes();
-//        String token = request.getRequest().getHeader("Authorization");
-//        if(token== null) throw new RuntimeException("token is null");
-//        token = token.replace("Bearer ","");
-//        System.out.println(authService.getClaims(token));
-//
-//
-//    }
-//
-//
-//
-//
-//}
+@Aspect
+@RequiredArgsConstructor
+public class TokenAspect {
+    private final TokenService tokenService;
+    @Before("@annotation(TokenRequired)")
+    public void TokenCheck(){
+        //Todo 토큰 없으면 로그인 페이지로 가게함
+        ServletRequestAttributes request =
+                (ServletRequestAttributes) RequestContextHolder
+                        .currentRequestAttributes();
+        String token = request.getRequest().getHeader("Authorization");
+        if(token== null) throw new RuntimeException("token is null");
+
+    }
+}
