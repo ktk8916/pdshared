f213928785029e32c461a66820ba5b787490b197
ktk8916 <ktk8916@naver.com>
2023-08-03 10:30:24 +0900

    멤버 아이디를 토큰으로 받아오도록 수정

--- src/main/java/com/playdata/pdshared/domain/group/controller/GroupController.java
+++ src/main/java/com/playdata/pdshared/domain/group/controller/GroupController.java
@@ -1,5 +1,7 @@
 package com.playdata.pdshared.domain.group.controller;
 
+import com.playdata.pdshared.config.service.OauthService;
+import com.playdata.pdshared.config.token.TokenService;
 import com.playdata.pdshared.domain.group.domain.request.TeamRequest;
 import com.playdata.pdshared.domain.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
@@ -11,19 +13,23 @@ import org.springframework.web.bind.annotation.*;
 public class GroupController {
 
     private final GroupService groupService;
+    private final TokenService tokenService;
 
     @PostMapping
-    public void createTeam(@RequestBody TeamRequest teamRequest){
-        // 멤버 아이디를 받아와서 같이 처리해야함
-        // 일단은 1L로 고정
-        groupService.createTeam(1L, teamRequest);
+    public void createTeam(
+            @RequestHeader("Authorization") String token,
+            @RequestBody TeamRequest teamRequest){
+        Long memberId = tokenService.getMemberIdByToken(token);
+        groupService.createTeam(memberId, teamRequest);
     }
 
     @PostMapping("/{teamId}/invite/{memberId}")
     public void invite(
+            @RequestHeader("Authorization") String token,
             @PathVariable("teamId") Long teamId,
             @PathVariable("memberId") Long memberId){
-        groupService.invite(teamId, memberId);
+        Long ownerId = tokenService.getMemberIdByToken(token);
+        groupService.invite(ownerId, teamId, memberId);
     }
 
 
