91256456deb139b13dc208548fef417dc1157c3e
ajtwu1235 <ajtwu1235@gmail.com>
2023-08-03 09:58:46 +0900

    0803

--- src/test/java/com/playdata/pdshared/domain/board/controller/BoardControllerTest.java
+++ src/test/java/com/playdata/pdshared/domain/board/controller/BoardControllerTest.java
@@ -11,8 +11,10 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -58,39 +61,42 @@ class BoardControllerTest {
         //given
         BoardRequest boardRequest = new BoardRequest("내용","제목", ViewType.PUBLIC,null);
 
+        String s = objectMapper.writeValueAsString(boardRequest);
+        MockMultipartFile board = new MockMultipartFile(
+                "req", "req.json", "application/json", s.getBytes(StandardCharsets.UTF_8));
         MockMultipartFile file = new MockMultipartFile(
                 "file", "test.txt", "text/plain", "Hello, World!".getBytes());
+
 
         //when and then
 
         mockMvc.perform(
-                        multipart("/board")
+                        multipart(HttpMethod.POST,"/board")
                                 .file(file)
+                                .file(board)
                                 .header("Authorization", "your-auth-token")
-                                .content(objectMapper.writeValueAsString(boardRequest))
-                                .contentType(MediaType.APPLICATION_JSON)
+
+//                                .content(objectMapper.writeValueAsString(boardRequest))
+//                                .contentType(MediaType.APPLICATION_JSON)
                 )
+
                 .andExpect(status().isOk())
                 .andDo(document("board/save",
                         requestHeaders(
                                 headerWithName("Authorization").description("Authorization token")
                         ),
                         requestParts(
-                                partWithName("file").description("The uploaded file")
+                                partWithName("file").description("업로드된 파일"),
+                                partWithName("req").description("JSON 형식의 BoardRequest 객체")
                         ),
                         requestPartFields("req",
-                                // Define fields for the BoardRequest object
-                                fieldWithPath("field1").description("Description for Field 1"),
-                                fieldWithPath("field2").description("Description for Field 2")
-                                // Add more fields as needed
-                        ),
-                        responseFields(
-                                // Define response fields
+                                fieldWithPath("content").description("BoardRequest의 내용 필드에 대한 설명"),
+                                fieldWithPath("title").description("BoardRequest의 제목 필드에 대한 설명"),
+                                fieldWithPath("viewType").description("BoardRequest의 viewType 필드에 대한 설명").optional(),
+                                fieldWithPath("hashtags").description("BoardRequest의 additionalProperty 필드에 대한 설명").optional()
                         )
                 ));
 
-
-        ;
 
 
     }
